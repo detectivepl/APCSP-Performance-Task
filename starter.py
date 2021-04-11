5f637295d2d55404ee3c3ba6752d5a9e7d254d44
detectivepl <chiwoonlee@gmail.com>
2021-04-11 12:51:27 -0700

    Set up testing window
    -Still need more icons
    -Work on matching img with img name

--- starter.py
+++ starter.py
@@ -1,5 +1,14 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from numpy import random
+
+# global questions
+# questions = ["car", "dog", "cat", "chair", "book", "paper"]
+# questions = ["car", "dog"]
+# kAnswers = ["차", "개", "고양이", "의자", "책", "종이"]
+kAnswers = ["차", "개"]
+# sAnswers = ["carro", "perro", "gato", "silla", "libro", "papel"]
+sAnswers = ["carro", "perro"]
 
 def introScreen():
     global canvas
@@ -11,23 +20,23 @@ def introScreen():
     entryText.grid(row=0, column=1)
     entryDesc = Label(canvas, text="Test your language skills here!", font="Times 16")
     entryDesc.grid(row=1, column=1)
-
+    #Korean
     koreanFlag = ImageTk.PhotoImage(Image.open("images\south-korean-flag.png").resize((300,200), Image.ANTIALIAS))
     kF = Label(canvas, image=koreanFlag)
     kF.image = koreanFlag
     kF.grid(row=2, column=0)
     koreanHello = Label(canvas, text="안녕하세요!", font="Times 32")
     koreanHello.grid(row=3, column=0)
-    koreanButton = Button(canvas, text="Korean", font="Times 32", padx=100, command=lambda:[resetScreen(), langKor(), testScreen()])
+    koreanButton = Button(canvas, text="Korean", font="Times 32", padx=100, borderwidth=5, command=lambda:[resetScreen(), langKor(), testScreen()])
     koreanButton.grid(row=4, column=0)
-
+    #Spanish
     spanishFlag = ImageTk.PhotoImage(Image.open("images\spanish-flag.png").resize((300,200), Image.ANTIALIAS))
     sF = Label(canvas, image=spanishFlag)
     sF.image = spanishFlag
     sF.grid(row=2, column=3)
     spanishHello = Label(canvas, text="¡Hola!", font="Times 32")
     spanishHello.grid(row=3, column=3)
-    spanishButton = Button(canvas, text="Spanish", font="Times 32", padx=100, command=lambda:[resetScreen(), langSpan(), testScreen()])
+    spanishButton = Button(canvas, text="Spanish", font="Times 32", padx=100, borderwidth=5, command=lambda:[resetScreen(), langSpan(), testScreen()])
     spanishButton.grid(row=4, column=3)
     
 def langKor():
@@ -42,22 +51,42 @@ def testScreen():
     global canvas
     canvas = Canvas(root)
     canvas.grid(row=0, column=0)
+    top = Label(canvas, text="")
+    top.grid(row=0, column=0, columnspan=3)
+    #Back Button
+    backButton = Button(canvas, text="Go back", borderwidth=5, command=lambda:[resetScreen(), introScreen()])
+    backButton.grid(row=0,column=0)
     
-    if langSwitch:
-        lawlll = Label(canvas, text="yoyoyoyoyoyooyo")
-        lawlll.grid(row=0, column=0)
-    else:
-        lawlll = Label(canvas, text="dsafsadfdsaf")
-        lawlll.grid(row=0, column=0)
+    questions = ["car", "dog"]
+    qImgs = []
+    for i in questions:
+        tempImg = ImageTk.PhotoImage(Image.open("images\\" + i + ".png"))
+        tI = Label(canvas, image=tempImg)
+        tI.image = tempImg
+        qImgs.append(tI)
     
-    car = ImageTk.PhotoImage(Image.open("images\car.png"))
-    carr = Label(canvas, image=car)
-    carr.image = car
-    carr.grid(row=2, column=3)
+    print(qImgs)
     
-    #CHANGE THIS 
-    bton = Button(canvas, text="Goback", command=lambda:[resetScreen(), introScreen()])
-    bton.grid(row=1,column=1)
+    while len(questions)>0:
+        randVal = random.randint(len(questions))
+        print(randVal)
+        if langSwitch:
+            imgName = Label(canvas, text=questions[randVal])
+            imgName.grid(row=0, column=1)
+            qImgs[randVal].grid(row=1, column=1)
+            questions.pop(randVal)
+            qImgs.pop(randVal)
+        else:
+            imgName = Label(canvas, text="dsafsadfdsaf")
+            imgName.grid(row=0, column=1)
+    
+    
+    # qImgs[0].grid(row=1, column=1)
+    answerBox = Entry(canvas, width=30, borderwidth=5)
+    answerBox.grid(row=2, column=1)
+    # enterButton = Button(canvas, text="Enter", borderwidth=5, command)
+    
+
     
 def resetScreen():
     canvas.destroy()
@@ -71,7 +100,7 @@ root.mainloop()
 
 #Images from:
 #   pixabay.com (Clker-Free-Vector-Images, deMysticWay)
-#   flaticon.com (Vectors Market, )
+#   flaticon.com (Vectors Market, Freepik, )
 
 
 
