f2ef4e1e7902c9047235353e67e307bbb39e8a68
detectivepl <chiwoonlee@gmail.com>
2021-05-20 02:37:36 -0700

    FINAL

--- TypingTest.py
+++ TypingTest.py
@@ -1,18 +1,13 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-#This reads the sentenceList text file and puts all the sentences in a list
 sentenceListTXT = open("sentenceList.txt", "r")
 sentenceList = sentenceListTXT.read().split("\n")
 sentenceListTXT.close()
 
-#This is the variable for the user's score in the beginning of the game
 score = 0
 
 def generateQuestion(difficulty):
-    #This generates a question based on the given difficulty
-    #If "easy", the question is a sentence with less than or equal to 10 words
-    #If "hard", the question is a sentence with more than 10 words
     newCanvas()
     global question
     global answerBox
@@ -50,9 +45,6 @@ def generateQuestion(difficulty):
         winScreen()
 
 def enterValue():
-    #This registers the user input
-    #If the user answers correctly, the test continues
-    #If the user answers incorrectly, the test ends
     global ans
     ans = answerBox.get()
     resetScreen()
@@ -74,13 +66,11 @@ def findError(lastQuestion, lastAnswer):
     return 3
 
 def newCanvas():
-    #This creates a new canvas
     global canvas
     canvas = Canvas(root)
     canvas.grid(row=0, column=0)
 
 def resetScreen():
-    #This removes everything on the page
     canvas.destroy()
 
 def winScreen():
@@ -92,13 +82,12 @@ def winScreen():
     v2.image = rightV
     v1.grid(row=0, column=0)
     v2.grid(row=0, column=2)
-    winMessage = Label(canvas, text="Nice Job!", font="Times 71")
+    winMessage = Label(canvas, text="Nice Job!", font="Times 40")
+    endScore = Label(canvas, text="All correct", font="Times 20")
     winMessage.grid(row=0, column=1)
-    endScore = Label(canvas, text="All correct", font="Times 40")
     endScore.grid(row=1, column=1)
 
 def loseScreen():
-    #This displays the user's score at the end of the test
     newCanvas()
     wrongX = ImageTk.PhotoImage(Image.open("wrong.png"))
     x1 = Label(canvas, image=wrongX)
@@ -108,7 +97,7 @@ def loseScreen():
     x1.grid(row=0, column=0)
     x2.grid(row=0, column=2)
 
-    box = LabelFrame(canvas, padx=100, pady=20)
+    box = LabelFrame(canvas, padx=55, pady=20)
     box.grid(row=1, column=1)
 
     errorCode = findError(question, ans)
@@ -119,12 +108,12 @@ def loseScreen():
     else:
         errorTypeMessage = "You made a typo!"
 
-    endMessage = Label(canvas, text=errorTypeMessage, font="Times 71")
-    lastq = Label(box, text="Sentence: ", font="Times 20")
-    lastQuestion = Label(box, text=question, font="Times 20")
-    lasta = Label(box, text="Your answer: ", font="Times 20")
-    lastAnswer = Label(box, text=ans, font="Times 20")
-    endScore = Label(canvas, text="Final score: " + str(score), font="Times 40")
+    endMessage = Label(canvas, text=errorTypeMessage, font="Times 40")
+    lastq = Label(box, text="Sentence: ", font="Times 16")
+    lastQuestion = Label(box, text=question, font="Times 16")
+    lasta = Label(box, text="Your answer: ", font="Times 16")
+    lastAnswer = Label(box, text=ans, font="Times 16")
+    endScore = Label(canvas, text="Final score: " + str(score), font="Times 20")
 
     endMessage.grid(row=0, column=1)
     lastq.grid(row=1, column=0)
@@ -134,14 +123,12 @@ def loseScreen():
     endScore.grid(row=3, column=1)
 
 def startTest():
-    #This starts the test with an easy question
     generateQuestion("easy")
 
 
-#PSVM
 root = Tk()
 root.title("Typing Test")
-# root.geometry("950x500")
+root.geometry("950x500")
 root.grid_columnconfigure(10, minsize=100)
 root.grid_rowconfigure(10, minsize=100)
 startTest()
